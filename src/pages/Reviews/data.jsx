d9e7dbb53c02803cc6e7bc79c689cafb47c3eed6
moazkarem <moazkare7@gmail.com>
2025-07-07 16:51:41 +0300

    fix review data component

--- src/pages/Reviews/data.jsx
+++ src/pages/Reviews/data.jsx
@@ -1,13 +1,13 @@
-// import { MdDelete } from "react-icons/md";
-// import img1 from "../../../public/images/brands/img1.png";
-// import img2 from "../../../public/images/brands/img3.png";
-// import img3 from "../../../public/images/brands/img3.png";
-// import img4 from "../../../public/images/brands/img4.png";
-// import img5 from "../../../public/images/brands/img5.png";
-// import img6 from "../../../public/images/brands/img6.png";
-// import img7 from "../../../public/images/brands/img7.png";
-// import img9 from "../../../public/images/brands/img9.png";
-// import img10 from "../../../public/images/brands/img10.png";
+import { MdDelete } from "react-icons/md";
+import img1 from "../../../public/images/brands/img1.png";
+import img2 from "../../../public/images/brands/img3.png";
+import img3 from "../../../public/images/brands/img3.png";
+import img4 from "../../../public/images/brands/img4.png";
+import img5 from "../../../public/images/brands/img5.png";
+import img6 from "../../../public/images/brands/img6.png";
+import img7 from "../../../public/images/brands/img7.png";
+import img9 from "../../../public/images/brands/img9.png";
+import img10 from "../../../public/images/brands/img10.png";
 
 export const images = [img1, img2, img3, img4, img5, img6, img7, img9, img10];
 export const getCategoryColumns = ({ onDelete }) => [
